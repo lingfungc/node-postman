d0db9d6dc45b551c10652594a258eff38ba788d2
Ling Fung Chan <justinlfchan@gmail.com>
2023-08-17 20:50:50 +0100

    added event-listener and add-key-value-pair function for query-params section  in script.js

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const keyValueTemplate = document.querySelector("[data-key-value-template]");
 const createKeyValuePair = () => {
-  const element = keyValueTemplate.textContent.cloneNode(true);
+  // * The .cloneNode(true) helps to clone all the text content from keyValueTemplate into variable "element"
+  const element = keyValueTemplate.content.cloneNode(true);
+
+  element.querySelector("[data-remove-btn]").addEventListener("click", (e) => {
+    e.target.closest("[data-key-value-pair]").remove();
+  });
+
+  return element;
 };
 
 const queryParamsContainer = document.querySelector("[data-query-params]");
+const queryParamsAddBtn = document.querySelector("[data-add-query-params-btn]");
+queryParamsAddBtn.addEventListener("click", () => {
+  queryParamsContainer.append(createKeyValuePair());
+});
 
 const requestHeadersContainer = document.querySelector(
   "[data-request-headers]"
 );
-
-const keyValueTemplate = document.querySelector("[date-key-value-template]");
-
-queryParamsContainer.append(createKeyValuePair());
-
 requestHeadersContainer.append(createKeyValuePair());
