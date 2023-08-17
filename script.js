850d9e7e85626c2590351d1f438418514a26ebdf
Ling Fung Chan <justinlfchan@gmail.com>
2023-08-17 20:41:05 +0100

    selected data-elements from dom in script.js

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+const createKeyValuePair = () => {
+  const element = keyValueTemplate.textContent.cloneNode(true);
+};
+
+const queryParamsContainer = document.querySelector("[data-query-params]");
+
+const requestHeadersContainer = document.querySelector(
+  "[data-request-headers]"
+);
+
+const keyValueTemplate = document.querySelector("[date-key-value-template]");
+
+queryParamsContainer.append(createKeyValuePair());
+
+requestHeadersContainer.append(createKeyValuePair());
